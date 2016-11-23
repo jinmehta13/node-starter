c99cb5b1476e35772e77da67834b2f1c911bdc69
Jinesh Mehta <jinmehta13@gmail.com>
2016-11-23 00:58:54 -0500

    created app instance via express and set default paths for views and static files

---
+++ project/app/server.js
@@ -0,0 +1,15 @@
+//dependencies
+var express = require('express');
+
+//create an express instance
+var app = express();
+
+//set view engine to ejs and default view directory
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/../public/views');
+
+//middleware to define path to public directory
+app.use(express.static(__dirname + '/../public'));
+
+//export app server
+module.exports = app;
---
+++ project/main.js
@@ -0,0 +1,6 @@
+//dependencies
+var app = require('./app/server');
+
+app.listen(8080, function() {
+	console.log('server is running');
+});
