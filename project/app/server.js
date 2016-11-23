317bceca3772e0755c1a377d5644d56714ecb2b4
Jinesh Mehta <jinmehta13@gmail.com>
2016-11-23 01:09:16 -0500

    added basic routes for home, top, results, and API pages

--- project/app/server.js
+++ project/app/server.js
@@ -11,5 +11,8 @@ app.set('views', __dirname + '/../public/views');
 //middleware to define path to public directory
 app.use(express.static(__dirname + '/../public'));
 
+//import routes
+require('./routes.js')(app);
+
 //export app server
 module.exports = app;
